115e3382d3985379ce25ab095baf495158d1fa62
Italo Spina <italo.spina@hotmail.com>
2024-03-29 14:51:00 -0300

    Projeto De Estacionamento

---
+++ ProjetosEstacionamento/ProjetosEstacionamento/Apresentar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetosEstacionamento
+{
+    public class Apresentar
+    {
+        public double precoInicial = 5;
+        public double precoPorHora = 2;
+        public double horaPermanecida;
+        public double valorReais;
+
+        public void Apesentacao()
+        {
+            Console.WriteLine("================================================");
+            Console.WriteLine("Bem vindo ao sistema de Estacionamentos Italo.\n");
+            Console.WriteLine("================================================");
+
+            Console.WriteLine($"Valor inicial: R${precoInicial},00");
+            //precoInicial = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"O Valor digitado foi: {precoInicial}");
+
+            Console.WriteLine($"Preco por hora: R${precoPorHora},00");
+            //precoPorHora = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"O Valor digitado foi: {precoInicial}");
+        }
+    }
+}
---
+++ ProjetosEstacionamento/ProjetosEstacionamento/Program.cs
@@ -0,0 +1,16 @@
+﻿using ProjetosEstacionamento;
+/// <summary>
+/// Parte um do projeto
+/// </summary>
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Apresentar apresentar = new Apresentar();
+        apresentar.Apesentacao();
+
+        Menu menu = new Menu();
+        menu.ApesentarMenu();
+
+    }
+}
